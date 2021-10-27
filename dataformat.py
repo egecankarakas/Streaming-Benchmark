b4673e8bdbae7d47a33df43e69c9079aba96adad
Tristan <tristan@cfurogyn.com>
2021-10-27 12:27:25 +0200

    add python script and requirments - for format our data

---
+++ dataformat.py
@@ -0,0 +1,70 @@
+import os
+import sys
+import getopt
+import pandas as pd
+import numpy as np
+
+# options System, #Nodes,
+
+countOfEvents = 'seen.txt'
+latancyTime = 'updated.txt'
+
+
+
+
+def createDataFile(numbNode = 1 , system = 'spark', testNumber = '1'):
+    
+
+    array_countOfEvent = np.loadtxt('seen.txt')
+    array_latancyTime = np.loadtxt(latancyTime)
+    array_NumbNode = np.full(array_countOfEvent.size, numbNode)
+    array_testNumber = np.full(array_countOfEvent.size, testNumber)
+    array_system = np.full(array_countOfEvent.size, system)
+
+
+
+    df = pd.DataFrame(data=[array_countOfEvent, array_latancyTime, array_NumbNode, array_testNumber, array_system]).T
+    #df.columns(['countOfEvent','latancyTime','#Nodes','testNubber','systems'])
+    #print(df)
+
+    df.to_csv("test_Nodes-{}_System-{}_Test-{}.csv".format(numberOfNodes,system,testnumber))
+
+    return
+
+def main(argv):
+    numberOfNodes = ''
+    system = ''
+    testnumber = ''
+    
+    try:
+        opts, args = getopt.getopt(argv, "h:#:s:t:",["help=","#Node=","System=","testNumber="])
+    except getopt.GetoptError:
+        print("help")
+        sys.exit()
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print("required flags \n -# or #Node - number of nodes \n s or System - spark or flink \n t or testNumber - for test")
+            sys.exit()
+        elif opt in ("-#","--#Node"):
+            numberOfNodes = arg
+        elif opt is "spark":
+            if args in ("sp","ar"):
+                system = "spark"
+            else: 
+                system = "flink"
+            print(system)
+        elif opt in ("-t","testNumber"):
+            #an idicator of what test we are doing
+            testnumber = arg
+    return numberOfNodes, system, testnumber
+
+
+
+if __name__ == "__main__":
+
+    numberOfNodes, system, testnumber = main(sys.argv[1:]) 
+    createDataFile(numberOfNodes, system, testnumber)
+
+
+
