475e2ea7ee20fcfac25405bd78509d532589aa5b
Tristan <tristan@cfurogyn.com>
2021-10-29 16:16:38 +0200

    returned kafka

--- stream-bench.sh
+++ stream-bench.sh
@@ -239,8 +239,8 @@ run() {
     echo 'Start Setup Spark'
     echo 'kafka.brokers:' >> $CONF_FILE
     echo 'try 2'
-    #kafka_brokers=$(getent hosts $KAFKA_NODES | awk '{ print $1}' | paste -sd " " -)
-    kafka_brokers = 'node55.ib.cluster'
+    kafka_brokers=$(getent hosts $KAFKA_NODES | awk '{ print $1}' | paste -sd " " -)
+    #kafka_brokers = 'node55.ib.cluster'
     echo '    - "'$kafka_brokers'"' >> $CONF_FILE
     echo >> $CONF_FILE
     echo "here"
